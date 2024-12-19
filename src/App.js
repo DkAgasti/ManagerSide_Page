f48b3f66870709599114330f0dfc8d5909604d0e
web2bariflo <web2.bariflo@outlook.com>
2024-12-19 17:55:48 +0530

    folder added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import SetTime from './managerside/SetTime'
 
 const App = () => {
   return (
-    <div>hii dk ! there me. </div>
+    <>
+    <SetTime/>
+    </>
   )
 }
 
